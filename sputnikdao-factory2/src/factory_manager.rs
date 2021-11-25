befb4b73af5b55f88a740a3ef226da364a3d86b3
Illia Polosukhin <ilblackdragon@gmail.com>
2021-11-25 22:05:03 +0000

    Factory can store any amount of contracts. Owner can set the current hash for deployment.

---
+++ sputnikdao-factory2/src/factory_manager.rs
@@ -0,0 +1,128 @@
+//! Module for standard generic contract factory manager.
+
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::json_types::Base58CryptoHash;
+use near_sdk::serde_json;
+use near_sdk::{env, sys, AccountId, Balance, CryptoHash, Gas};
+
+/// Gas spent on the call & account creation.
+const CREATE_CALL_GAS: Gas = Gas(75_000_000_000_000);
+
+/// Gas allocated on the callback.
+const ON_CREATE_CALL_GAS: Gas = Gas(10_000_000_000_000);
+
+const NO_DEPOSIT: Balance = 0;
+
+/// Factory manager that allows to store/load contracts by hash directly in the storage.
+/// Uses directly underlying host functions to not load any of the data into WASM memory.
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct FactoryManager {}
+
+impl FactoryManager {
+    /// Store contract from input.
+    pub fn store_contract(&self) {
+        unsafe {
+            // Load input into register 0.
+            sys::input(0);
+            // Compute sha256 hash of register 0 and store in 1.
+            sys::sha256(u64::MAX as _, 0 as _, 1);
+            // Check if such blob already stored.
+            assert_eq!(
+                sys::storage_has_key(u64::MAX as _, 1 as _),
+                0,
+                "ERR_ALREADY_EXISTS"
+            );
+            // Store value of register 0 into key = register 1.
+            sys::storage_write(u64::MAX as _, 1 as _, u64::MAX as _, 0 as _, 2);
+            // Load register 1 into blob_hash.
+            let blob_hash = [0u8; 32];
+            sys::read_register(1, blob_hash.as_ptr() as _);
+            // Return from function value of register 1.
+            let blob_hash_str = serde_json::to_string(&Base58CryptoHash::from(blob_hash))
+                .unwrap()
+                .into_bytes();
+            sys::value_return(blob_hash_str.len() as _, blob_hash_str.as_ptr() as _);
+        }
+    }
+
+    /// Delete code from the contract.
+    pub fn delete_contract(&self, code_hash: Base58CryptoHash) {
+        let code_hash: CryptoHash = code_hash.into();
+        env::storage_remove(&code_hash);
+    }
+
+    /// Get code for given hash.
+    pub fn get_code(&self, code_hash: Base58CryptoHash) {
+        let code_hash: CryptoHash = code_hash.into();
+        unsafe {
+            // Check that such contract exists.
+            assert_eq!(
+                sys::storage_has_key(code_hash.len() as _, code_hash.as_ptr() as _),
+                1,
+                "Contract doesn't exist"
+            );
+            // Load the hash from storage.
+            sys::storage_read(code_hash.len() as _, code_hash.as_ptr() as _, 0);
+            // Return as value.
+            sys::value_return(u64::MAX as _, 0 as _);
+        }
+    }
+
+    /// Create given contract with args and callback factory.
+    pub fn create_contract(
+        &self,
+        code_hash: Base58CryptoHash,
+        account_id: AccountId,
+        new_method: &str,
+        args: &[u8],
+        callback_method: &str,
+        callback_args: &[u8],
+    ) {
+        let code_hash: CryptoHash = code_hash.into();
+        let attached_deposit = env::attached_deposit();
+        let factory_account_id = env::current_account_id().as_bytes().to_vec();
+        let account_id = account_id.as_bytes().to_vec();
+        unsafe {
+            // Check that such contract exists.
+            assert_eq!(
+                sys::storage_has_key(code_hash.len() as _, code_hash.as_ptr() as _),
+                1,
+                "Contract doesn't exist"
+            );
+            // Load input (wasm code) into register 0.
+            sys::storage_read(code_hash.len() as _, code_hash.as_ptr() as _, 0);
+            // schedule a Promise tx to account_id
+            let promise_id =
+                sys::promise_batch_create(account_id.len() as _, account_id.as_ptr() as _);
+            // create account first.
+            sys::promise_batch_action_create_account(promise_id);
+            // transfer attached deposit.
+            sys::promise_batch_action_transfer(promise_id, &attached_deposit as *const u128 as _);
+            // deploy contract (code is taken from register 0).
+            sys::promise_batch_action_deploy_contract(promise_id, u64::MAX as _, 0);
+            // call `new` with given arguments.
+            sys::promise_batch_action_function_call(
+                promise_id,
+                new_method.len() as _,
+                new_method.as_ptr() as _,
+                args.len() as _,
+                args.as_ptr() as _,
+                &NO_DEPOSIT as *const u128 as _,
+                CREATE_CALL_GAS.0,
+            );
+            // attach callback to the factory.
+            let _ = sys::promise_then(
+                promise_id,
+                factory_account_id.len() as _,
+                factory_account_id.as_ptr() as _,
+                callback_method.len() as _,
+                callback_method.as_ptr() as _,
+                callback_args.len() as _,
+                callback_args.as_ptr() as _,
+                &NO_DEPOSIT as *const u128 as _,
+                ON_CREATE_CALL_GAS.0,
+            );
+            sys::promise_return(promise_id);
+        }
+    }
+}
--- sputnikdao-factory2/src/lib.rs
+++ sputnikdao-factory2/src/lib.rs
@@ -1,45 +1,83 @@
+mod factory_manager;
+
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedSet;
-use near_sdk::json_types::{Base64VecU8, U128};
-use near_sdk::{assert_self, env, ext_contract, near_bindgen, AccountId, Gas, Promise, PublicKey};
+use near_sdk::json_types::{Base58CryptoHash, Base64VecU8, U128};
+use near_sdk::serde_json::{self, json};
+use near_sdk::{env, near_bindgen, AccountId, CryptoHash, PanicOnDefault, Promise};
 
-const CODE: &[u8] = include_bytes!("../../sputnikdao2/res/sputnikdao2.wasm");
+use factory_manager::FactoryManager;
 
-/// Gas spent on the call & account creation.
-const CREATE_CALL_GAS: Gas = Gas(75_000_000_000_000);
-
-/// Gas allocated on the callback.
-const ON_CREATE_CALL_GAS: Gas = Gas(10_000_000_000_000);
-
-#[ext_contract(ext_self)]
-pub trait ExtSelf {
-    fn on_create(
-        &mut self,
-        account_id: AccountId,
-        attached_deposit: U128,
-        predecessor_account_id: AccountId,
-    ) -> bool;
-}
+const LATEST_CODE_HASH_KEY: &[u8; 4] = b"CODE";
+const OWNER_KEY: &[u8; 5] = b"OWNER";
 
 #[near_bindgen]
-#[derive(BorshSerialize, BorshDeserialize)]
+#[derive(BorshSerialize, BorshDeserialize, PanicOnDefault)]
 pub struct SputnikDAOFactory {
+    factory_manager: FactoryManager,
     daos: UnorderedSet<AccountId>,
-}
-
-impl Default for SputnikDAOFactory {
-    fn default() -> Self {
-        env::panic_str("SputnikDAOFactory should be initialized before usage")
-    }
 }
 
 #[near_bindgen]
 impl SputnikDAOFactory {
     #[init]
     pub fn new() -> Self {
-        assert!(!env::state_exists(), "The contract is already initialized");
         Self {
+            factory_manager: FactoryManager {},
             daos: UnorderedSet::new(b"d".to_vec()),
+        }
+    }
+
+    pub fn set_owner(&self, owner_id: AccountId) {
+        self.assert_owner();
+        env::storage_write(OWNER_KEY, owner_id.as_bytes());
+    }
+
+    pub fn set_code_hash(&self, code_hash: Base58CryptoHash) {
+        self.assert_owner();
+        let code_hash: CryptoHash = code_hash.into();
+        env::storage_write(LATEST_CODE_HASH_KEY, &code_hash);
+    }
+
+    pub fn delete_contract(&self, code_hash: Base58CryptoHash) {
+        self.assert_owner();
+        self.factory_manager.delete_contract(code_hash);
+    }
+
+    #[payable]
+    pub fn create(&mut self, name: AccountId, args: Base64VecU8) {
+        let account_id: AccountId = format!("{}.{}", name, env::current_account_id())
+            .parse()
+            .unwrap();
+        let callback_args = serde_json::to_vec(&json!({
+            "account_id": account_id,
+            "attached_deposit": U128(env::attached_deposit()),
+            "predecessor_account_id": env::predecessor_account_id()
+        }))
+        .expect("Failed to serialize");
+        self.factory_manager.create_contract(
+            self.get_latest_code_hash(),
+            account_id,
+            "new",
+            &args.0,
+            "on_create",
+            &callback_args,
+        );
+    }
+
+    #[private]
+    pub fn on_create(
+        &mut self,
+        account_id: AccountId,
+        attached_deposit: U128,
+        predecessor_account_id: AccountId,
+    ) -> bool {
+        if near_sdk::is_promise_success() {
+            self.daos.insert(&account_id);
+            true
+        } else {
+            Promise::new(predecessor_account_id).transfer(attached_deposit.0);
+            false
         }
     }
 
@@ -60,79 +98,86 @@ impl SputnikDAOFactory {
             .collect()
     }
 
-    #[payable]
-    pub fn create(
-        &mut self,
-        name: AccountId,
-        public_key: Option<PublicKey>,
-        args: Base64VecU8,
-    ) -> Promise {
-        let account_id: AccountId = format!("{}.{}", name, env::current_account_id())
-            .parse()
-            .unwrap();
-        let mut promise = Promise::new(account_id.clone())
-            .create_account()
-            .deploy_contract(CODE.to_vec())
-            .transfer(env::attached_deposit());
-        if let Some(key) = public_key {
-            promise = promise.add_full_access_key(key.into())
-        }
-        promise
-            .function_call(
-                "new".to_string(),
-                args.into(),
-                0,
-                env::prepaid_gas() - CREATE_CALL_GAS - ON_CREATE_CALL_GAS,
+    pub fn get_owner(&self) -> AccountId {
+        AccountId::new_unchecked(
+            String::from_utf8(
+                env::storage_read(OWNER_KEY)
+                    .unwrap_or(env::current_account_id().as_bytes().to_vec()),
             )
-            .then(ext_self::on_create(
-                account_id,
-                U128(env::attached_deposit()),
-                env::predecessor_account_id(),
-                env::current_account_id(),
-                0,
-                ON_CREATE_CALL_GAS,
-            ))
+            .expect("INTERNAL_FAIL"),
+        )
     }
 
-    pub fn on_create(
-        &mut self,
-        account_id: AccountId,
-        attached_deposit: U128,
-        predecessor_account_id: AccountId,
-    ) -> bool {
-        assert_self();
-        if near_sdk::is_promise_success() {
-            self.daos.insert(&account_id);
-            true
-        } else {
-            Promise::new(predecessor_account_id).transfer(attached_deposit.0);
-            false
-        }
+    pub fn get_latest_code_hash(&self) -> Base58CryptoHash {
+        slice_to_hash(&env::storage_read(LATEST_CODE_HASH_KEY).expect("Must have code hash"))
     }
+
+    /// Returns non serialized code by given code hash.
+    pub fn get_code(&self, code_hash: Base58CryptoHash) {
+        self.factory_manager.get_code(code_hash);
+    }
+
+    fn assert_owner(&self) {
+        assert_eq!(
+            self.get_owner(),
+            env::predecessor_account_id(),
+            "Must be owner"
+        );
+    }
+}
+
+pub fn slice_to_hash(hash: &[u8]) -> Base58CryptoHash {
+    let mut result: CryptoHash = [0; 32];
+    result.copy_from_slice(&hash);
+    Base58CryptoHash::from(result)
+}
+
+/// Store new contract. Non serialized argument is the contract.
+/// Returns base58 of the hash of the contract.
+#[no_mangle]
+pub extern "C" fn store() {
+    env::setup_panic_hook();
+    let contract: SputnikDAOFactory = env::state_read().expect("Contract is not initialized");
+    let prev_storage = env::storage_usage();
+    contract.factory_manager.store_contract();
+    let storage_cost = (env::storage_usage() - prev_storage) as u128 * env::storage_byte_cost();
+    assert!(
+        storage_cost >= env::attached_deposit(),
+        "Must at least deposit {} to store",
+        storage_cost
+    );
 }
 
 #[cfg(test)]
 mod tests {
     use near_sdk::test_utils::{accounts, testing_env_with_promise_results, VMContextBuilder};
-    use near_sdk::{testing_env, PromiseResult};
+    use near_sdk::{testing_env, PromiseResult, VMContext};
 
     use super::*;
+
+    pub fn add_contract(
+        context: &mut VMContext,
+        contract: &mut SputnikDAOFactory,
+    ) -> Base58CryptoHash {
+        context.input = include_bytes!("../../sputnikdao2/res/sputnikdao2.wasm").to_vec();
+        let hash = slice_to_hash(&env::sha256(&context.input));
+        testing_env!(context.clone());
+        contract.factory_manager.store_contract();
+        context.input = vec![];
+        hash
+    }
 
     #[test]
     fn test_basics() {
         let mut context = VMContextBuilder::new();
         testing_env!(context.current_account_id(accounts(0)).build());
         let mut factory = SputnikDAOFactory::new();
+        let hash = add_contract(&mut context.build(), &mut factory);
+        testing_env!(context.predecessor_account_id(accounts(0)).build());
+        factory.set_code_hash(hash);
+
         testing_env!(context.attached_deposit(10).build());
-        factory.create(
-            "test".parse().unwrap(),
-            Some(
-                "ed25519:6E8sCci9badyRkXb3JoRpBj5p8C6Tw41ELDZoiihKEtp"
-                    .parse()
-                    .unwrap(),
-            ),
-            "{}".as_bytes().to_vec().into(),
-        );
+        factory.create("test".parse().unwrap(), "{}".as_bytes().to_vec().into());
         testing_env_with_promise_results(
             context.predecessor_account_id(accounts(0)).build(),
             PromiseResult::Successful(vec![]),
