2255ad216c62a1bee79806c02e553a41b7219803
Buckram123 <m.donchenko@4irelabs.com>
2021-12-10 12:55:11 +0200

    workspace init

---
+++ sputnikdao2/near-workspaces/__tests__/delegation.ts
@@ -0,0 +1,26 @@
+import { Workspace, BN, NearAccount, captureError, toYocto, tGas } from 'near-workspaces-ava';
+
+
+export const workspace = Workspace.init(async ({ root }) => {
+    const alice = await root.createAccount('alice');
+
+
+    const config = {name: 'test', purpose: 'testing', metadata: []};
+    
+    // Create a subaccount of the root account, and also deploy a contract to it
+    const dao = await root.createAndDeploy(
+      'dao',
+      '../res/sputnikdao2.wasm',
+      {
+          method: 'new',
+          args: {config, policy: []},
+      }
+    );
+  
+    return { alice, dao };
+  });
+  
+
+workspace.test('', async (test, { dao }) => {
+
+});
