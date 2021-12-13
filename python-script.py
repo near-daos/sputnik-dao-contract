2c0833d6d144a3cfc972d7c0e41be08a7738ae42
Constantin Dogaru <constantin.dogaru@mbntech.ro>
2021-12-13 13:19:50 +0200

    made store call work and also added delete call

--- python-script.py
+++ python-script.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3.10
 import base58
+import base64
 import hashlib
 import json
 import subprocess
@@ -23,30 +24,31 @@ subprocess.run([
     "--initFunction", "new", "--initArgs", init_args
 ])
 
-subprocess.run([
-    "wasm2wat", f"{SPUTNIK_REPO_PATH}/sputnikdao2/res/sputnikdao2.wasm", "-o",
-    f"{SPUTNIK_REPO_PATH}/sputnikdao2/res/sputnikdao2.wat"
-])
-with open(f"{SPUTNIK_REPO_PATH}/sputnikdao2/res/sputnikdao2.wat",
-          "r") as wat_file:
-    # TODO: wat_contract is too long for near cli
-    wat_contract = wat_file.read()  # read entire file as text
-    params = json.dumps({"input": wat_contract})
+with open(f"{SPUTNIK_REPO_PATH}/sputnikdao2/res/sputnikdao2.wasm",
+          "rb") as wasm_file:
+    wasm_contract = wasm_file.read()  # read entire file as bytes
+    wasm_contract_in_base64 = base64.b64encode(wasm_contract)
+
+    hash_in_hex = hashlib.sha256(wasm_contract).hexdigest()
+    hash_in_bytes = bytes.fromhex(hash_in_hex)
+    hash_in_base58 = base58.b58encode(hash_in_bytes).decode("UTF-8")
+
+    return_value = subprocess.run([
+        "near", "call", FACTORY_ACCOUNT, "store", wasm_contract_in_base64,
+        "--base64", "--accountId", FACTORY_ACCOUNT
+    ],
+                                  capture_output=True,
+                                  text=True).stdout.splitlines()[-1].strip("'")
+    assert return_value == hash_in_base58  # it means that the contract was stored successfully
+
+    params = json.dumps({"code_hash": hash_in_base58})
     subprocess.run([
-        "near", "call", FACTORY_ACCOUNT, "store", params, "--accountId",
-        FACTORY_ACCOUNT
+        "near", "call", FACTORY_ACCOUNT, "set_code_hash", params,
+        "--accountId", FACTORY_ACCOUNT
     ])
 
-# with open(f"{SPUTNIK_REPO_PATH}/sputnikdao2/res/sputnikdao2.wasm",
-#           "rb") as wasm_file:
-#     wasm_contract = wasm_file.read()  # read entire file as bytes
-
-#     hash_in_hex = hashlib.sha256(wasm_contract).hexdigest()
-#     hash_in_bytes = bytes.fromhex(hash_in_hex)
-#     hash_in_base58 = base58.b58encode(hash_in_bytes).decode("UTF-8")
-
-#     params = json.dumps({"code_hash": hash_in_base58})
-#     subprocess.run([
-#         "near", "call", FACTORY_ACCOUNT, "set_code_hash", params,
-#         "--accountId", FACTORY_ACCOUNT
-#     ])
+    params = json.dumps({"code_hash": hash_in_base58})
+    subprocess.run([
+        "near", "call", FACTORY_ACCOUNT, "delete_contract", params,
+        "--accountId", FACTORY_ACCOUNT
+    ])
