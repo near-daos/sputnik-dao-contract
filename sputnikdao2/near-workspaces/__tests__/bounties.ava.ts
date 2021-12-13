0914cb3c832df52f41e928f3bdc42ec89885b9d6
NinoLipartiia4ire <n.lipartiia@4irelabs.com>
2021-12-13 11:28:01 +0200

    Can add bounty

--- sputnikdao2/near-workspaces/__tests__/bounties.ava.ts
+++ sputnikdao2/near-workspaces/__tests__/bounties.ava.ts
@@ -1,6 +1,23 @@
 import { Workspace, BN, NearAccount, captureError, toYocto, tGas } from 'near-workspaces-ava';
 import { workspace, initStaking, initTestToken } from './utils';
 
-workspace.test('Bounty claim', async (test, {root, dao }) => {
-    
+workspace.test('Bounty claim', async (test, {alice, root, dao }) => {
+    const bounty = {
+        description: 'test_bounties',
+        token: alice,
+        amount: '1',
+        times: 2,
+        max_deadline: '1000'
+    }
+    await alice.call(dao, 'add_proposal', {
+        proposal: {
+            description: 'add_new_bounty',
+            kind: {
+                AddBounty: {
+                    bounty
+                }
+            }
+        },
+    },
+        { attachedDeposit: toYocto('1') })
 });
