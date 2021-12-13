931f5ce5c6e606fcb7e251e73d83b220dc00c971
NinoLipartiia4ire <n.lipartiia@4irelabs.com>
2021-12-13 15:31:36 +0200

    Can claim bounty

--- sputnikdao2/near-workspaces/__tests__/bounties.ava.ts
+++ sputnikdao2/near-workspaces/__tests__/bounties.ava.ts
@@ -1,13 +1,13 @@
 import { Workspace, BN, NearAccount, captureError, toYocto, tGas } from 'near-workspaces-ava';
-import { workspace, initStaking, initTestToken } from './utils';
+import { workspace, initStaking, initTestToken, STORAGE_PER_BYTE } from './utils';
 
-workspace.test('Bounty claim', async (test, {alice, root, dao }) => {
+async function proposeBounty(alice: NearAccount, dao: NearAccount) {  
     const bounty = {
         description: 'test_bounties',
         token: alice,
         amount: '1',
-        times: 2,
-        max_deadline: '1000'
+        times: 3,
+        max_deadline: '1925376849430593581'
     }
     await alice.call(dao, 'add_proposal', {
         proposal: {
@@ -19,5 +19,57 @@ workspace.test('Bounty claim', async (test, {alice, root, dao }) => {
             }
         },
     },
-        { attachedDeposit: toYocto('1') })
+        { 
+            attachedDeposit: toYocto('1') 
+        }
+    )
+}
+
+async function voteOnBounty(alice: NearAccount, dao: NearAccount) {
+    await alice.call(dao, 'act_proposal', 
+    {
+        id: 0,
+        action: 'VoteApprove'
+    })
+}
+
+async function claimBounty(alice: NearAccount, dao: NearAccount) {
+    await alice.call(dao, 'act_proposal', 
+    {
+        id: 0,
+        deadline: '1925376849430593581'
+
+    },
+    { 
+        attachedDeposit: toYocto('1') 
+    })
+}
+
+workspace.test('Bounty claim', async (test, {alice, root, dao }) => {
+    proposeBounty(alice, dao);
+    voteOnBounty(alice, dao);
+    claimBounty(alice, dao);
+});
+
+workspace.test('Bounty done', async (test, {alice, root, dao }) => {
+    const regCost = STORAGE_PER_BYTE.mul(new BN(16));
+
+    const testToken = await initTestToken(root);
+    const staking = await initStaking(root, dao, testToken);
+
+    proposeBounty(alice, dao);
+
+    let errorString = await captureError(async () =>
+        staking.call(dao, 'bounty_done', 
+        {
+            id: 1,
+            account_id: alice,
+            description: 'new_bounty_done'
+        },
+            { attachedDeposit: regCost }));
+    //test.regex(errorString, /ERR_BOUNTY_DONE_MUST_BE_SELF/);
+});
+
+workspace.test('Bounty giveup', async (test, {alice, root, dao }) => {
+    proposeBounty(alice, dao);
 });
