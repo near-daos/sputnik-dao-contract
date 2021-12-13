70dae2c97e03c0303fe01333abceb6365aa4bec1
NinoLipartiia4ire <n.lipartiia@4irelabs.com>
2021-12-13 09:57:31 +0200

    Renamed one file

---
+++ sputnikdao2/near-workspaces/__tests__/bounties.ava.ts
@@ -0,0 +1,6 @@
+import { Workspace, BN, NearAccount, captureError, toYocto, tGas } from 'near-workspaces-ava';
+import { workspace, initStaking, initTestToken } from './utils';
+
+workspace.test('Bounty claim', async (test, {root, dao }) => {
+    
+});
