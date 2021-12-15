7abacfc52b98812856dc03465db80832cc826997
Buckram123 <m.donchenko@4irelabs.com>
2021-12-15 16:21:12 +0200

    Self lock test added

--- sputnikdao2/tests-ava/__tests__/policy.ava.ts
+++ sputnikdao2/tests-ava/__tests__/policy.ava.ts
@@ -17,9 +17,9 @@ const workspace = Workspace.init(async ({ root }) => {
     return { alice, dao };
 });
 
-// FINISH THIS TEST
 workspace.test('Testing policy TokenWeight', async (test, { alice, root, dao }) => {
-    let config = { name: 'sputnik', purpose: 'testing', metadata: '' };
+    const config = { name: 'sputnik', purpose: 'testing', metadata: '' };
+    const bob = await root.createAccount('bob')
     const period = new BN('1000000000').muln(60).muln(60).muln(24).muln(7).toString();
     const testToken = await initTestToken(root);
     const staking = await initStaking(root, dao, testToken);
@@ -35,7 +35,7 @@ workspace.test('Testing policy TokenWeight', async (test, { alice, root, dao }) 
         roles: [
             {
                 name: "all",
-                kind: { "Group": [alice.accountId] }, // fails with kind: "Everyone" need to investigate
+                kind: { "Group": [alice.accountId, bob.accountId] }, // fails with kind: "Everyone" need to investigate
                 permissions: ["*:AddProposal",
                     "*:VoteApprove"],
                 vote_policy: {}
@@ -45,7 +45,7 @@ workspace.test('Testing policy TokenWeight', async (test, { alice, root, dao }) 
         {
             weight_kind: "TokenWeight",
             quorum: new BN('1').toString(),
-            threshold: '1',
+            threshold: '5',
         },
         proposal_bond: toYocto('1'),
         proposal_period: period,
@@ -82,6 +82,7 @@ workspace.test('Testing policy TokenWeight', async (test, { alice, root, dao }) 
         metadata: '',
     };
     await registerAndDelegate(dao, staking, alice, new BN('1'));
+    await registerAndDelegate(dao, staking, bob, new BN('4'));
     proposalId = await alice.call(
         dao,
         'add_proposal',
@@ -107,6 +108,75 @@ workspace.test('Testing policy TokenWeight', async (test, { alice, root, dao }) 
             action: 'VoteApprove',
         }
     );
+    await bob.call(
+        dao,
+        'act_proposal',
+        {
+            id: proposalId,
+            action: 'VoteApprove',
+        }
+    );
     test.deepEqual(await dao.view('get_config'),
         new_config);
 });
+
+
+workspace.test('Policy self-lock', async (test, { alice, root, dao }) => {
+    const config = { name: 'sputnik', purpose: 'testing', metadata: '' };
+    const period = new BN('1000000000').muln(60).muln(60).muln(24).muln(7).toString();
+    const policy =
+    {
+        roles: [
+            {
+                name: "all",
+                kind: { "Group": [alice.accountId] }, // fails with kind: "Everyone" need to investigate
+                permissions: ["*:AddProposal",
+                    "*:VoteApprove"],
+                vote_policy: {}
+            }
+        ],
+        default_vote_policy:
+        {
+            weight_kind: "TokenWeight",
+            quorum: new BN('1').toString(),
+            threshold: '5',
+        },
+        proposal_bond: toYocto('1'),
+        proposal_period: period,
+        bounty_bond: toYocto('1'),
+        bounty_forgiveness_period: period,
+    };
+    // 'staking_id' is not set, we can't delegate, so this contract got locked
+    await root.call(
+        dao,
+        'new',
+        { config, policy },
+    );
+    const proposalId = await alice.call(
+        dao,
+        'add_proposal',
+        {
+            proposal: {
+                description: 'test',
+                kind: {
+                    'ChangePolicy': {
+                        policy,
+                    }
+                },
+            }
+        },
+        {
+            attachedDeposit: toYocto('1'),
+        }
+    );
+    await alice.call(
+        dao,
+        'act_proposal',
+        {
+            id: proposalId,
+            action: 'VoteApprove',
+        }
+    );
+    let a:{status} = await dao.view('get_proposal', {id: proposalId});
+    test.is(a.status, 'InProgress');
+})
