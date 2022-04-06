c4cb726006860ffe134c79949da999ecace89af5
nninkoviSQA <nemanja.ninkovic@sqa-consulting.com>
2022-04-06 15:53:08 +0200

    added dao

--- sputnikdao-factory2/tests/utils/mod.rs
+++ sputnikdao-factory2/tests/utils/mod.rs
@@ -1,7 +1,7 @@
 near_sdk_sim::lazy_static_include::lazy_static_include_bytes! {
 
-    FACTORY_WASM_BYTES => "./res/sputnikdao_factory2.wasm",
-    TEST_TOKEN_WASM_BYTES => "../test-token/res/test_token.wasm",
+    FACTORY_WASM_BYTES => "res/sputnikdao_factory2.wasm",
+    DAO_WASM_BYTES => "../res/sputnikdao2.wasm",
 }
 
 // use near_sdk_sim::{deploy, init_simulator, to_yocto, ContractAccount, UserAccount};
