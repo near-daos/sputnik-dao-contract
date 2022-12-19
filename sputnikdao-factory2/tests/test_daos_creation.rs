393e9d992d8e7d56ebf54daff2faeb8f71eb85de
nninkovicSQA <nemanja.ninkovic@sqa-consulting.com>
2022-12-19 09:57:35 +0100

    Refactoring.

--- sputnikdao-factory2/tests/test_daos_creation.rs
+++ sputnikdao-factory2/tests/test_daos_creation.rs
@@ -1,49 +1,88 @@
-use anyhow::Ok;
-use near_sdk::{json_types::Base64VecU8, AccountId};
+use near_sdk::json_types::{Base58CryptoHash, Base64VecU8};
+use near_sdk::AccountId as AccID;
 use near_units::parse_near;
-use serde_json::json;
 use sputnikdao2::{Config, VersionedPolicy};
-
-const FACTORY_WASM: &[u8] = include_bytes!("../res/sputnikdao_factory2.wasm");
+use std::str::FromStr;
+use workspaces::types::{KeyType, SecretKey};
+use workspaces::AccountId;
 
 #[tokio::test]
-async fn daos_creation() -> anyhow::Result<()> {
+async fn init_and_default() -> anyhow::Result<()> {
+    // Create a sandbox environment.
     let worker = workspaces::sandbox().await?;
-    let contract = worker.dev_deploy(FACTORY_WASM).await?;
-    // let test_accoutnt = worker.dev_create_account().await?;
 
-    let res_new = contract.call("new").max_gas().transact().await?;
+    // Deploy Spunik DAO factory contract in sandbox
+    println!("Deploying Spunik DAO factory contract");
+    let wasm = std::fs::read("./res/sputnikdao_factory2.wasm")?;
+    let dao_factory = worker
+        .create_tla_and_deploy(
+            AccountId::from_str("dao-factory.test.near")?,
+            SecretKey::from_random(KeyType::ED25519),
+            &wasm,
+        )
+        .await?
+        .unwrap();
 
-    assert!(res_new.is_success());
+    println!("Contract Id: {:?}", dao_factory.id());
 
+    // Init daofactory contract
+    println!("Initializing daofactory contract");
+    let init_daofactory = dao_factory
+        .call("new")
+        .max_gas()
+        .transact()
+        .await?
+        .borsh()?;
+    println!("Initialization complete. {:?}", init_daofactory);
+
+    // Define parameters of new dao:
+
+    // Configure name, purpose, and initial council members of the DAO and convert the arguments in base64
     let config = Config {
-        name: "testdao".to_string(),
-        purpose: "to test".to_string(),
+        name: "sputnik-dao".to_string(),
+        purpose: "Sputnik internal test DAO".to_string(),
         metadata: Base64VecU8(vec![]),
     };
-    let policy = VersionedPolicy::Default(vec![AccountId::new_unchecked("testing".to_string())]);
-    let params = json!({ "config": config, "policy": policy })
+    let policy = VersionedPolicy::Default(vec![]);
+    let params = serde_json::json!({ "config": config, "policy": policy, })
         .to_string()
         .into_bytes();
 
-    let res_create = contract
+    // Create a new DAO
+    println!("Creating new DAO");
+    let create_new_dao = dao_factory
         .call("create")
-        .args_json(json!({
-                    "name":AccountId::new_unchecked("testdao".to_string()),
-                    "args":Base64VecU8(params),
+        .args_json(serde_json::json!({
+            "name": "createdao",
+            "args":Base64VecU8(params),
         }))
-        .max_gas()
         .deposit(parse_near!("6 N"))
+        .max_gas()
         .transact()
-        .await?;
+        .await?
+        .borsh()?;
+    println!("DAO successfully created. {:?}", create_new_dao);
 
-    assert!(res_create.is_success());
+    println!("Getting the factory owner...");
+    let get_owner = AccID::new_unchecked(dao_factory.view("get_owner").await?.json()?);
+    println!("Factory owner: {:?}", get_owner);
 
-    // let res_get_daos = contract.view("get_daos").await?;
+    println!("Getting default code hash...");
+    let get_default_code_hash: Base58CryptoHash =
+        dao_factory.view("get_default_code_hash").await?.json()?;
+    println!("Code Hash: {:?}", get_default_code_hash);
 
-    // assert!(!res_get_daos.result.is_empty());
+    println!("Getting daos list...");
+    let get_dao_list: Vec<AccountId> = dao_factory.view("get_dao_list").await?.json()?;
+    println!("List of DAOs {:?}", get_dao_list);
 
-    // will be updated after more research is dont on workspaces-rs v0.7.0
+    println!("Getting code...");
+    let get_code = dao_factory
+        .view("get_code")
+        .args_json(serde_json::json!({ "code_hash": get_default_code_hash, }))
+        .await?
+        .result;
+    println!("Code lenght: {:?}", get_code.len());
 
     Ok(())
 }
