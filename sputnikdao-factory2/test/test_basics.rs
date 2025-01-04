e3aed4712547ca8360532f7f763b734db0632a75
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-01-04 19:58:11 +0000

    codespace building

---
+++ sputnikdao-factory2/test/test_basics.rs
@@ -0,0 +1,29 @@
+
+
+#[tokio::test]
+async fn test_factory() -> Result<(), Box<dyn std::error::Error>> {
+    const SPUTNIKDAO_FACTORY_CONTRACT_ACCOUNT: &str = "sputnik-dao.near";
+
+    let mainnet = near_workspaces::mainnet().await?;
+    let sputnikdao_factory_contract_id: AccountId = SPUTNIKDAO_FACTORY_CONTRACT_ACCOUNT.parse()?;
+
+    let worker = near_workspaces::sandbox().await?;
+
+    let sputnik_dao_factory = worker
+        .import_contract(&sputnikdao_factory_contract_id, &mainnet)
+        .initial_balance(NearToken::from_near(1000))
+        .transact()
+        .await?;
+
+    let init_sputnik_dao_factory_result =
+        sputnik_dao_factory.call("new").max_gas().transact().await?;
+    if init_sputnik_dao_factory_result.is_failure() {
+        panic!(
+            "Error initializing sputnik-dao contract: {:?}",
+            String::from_utf8(init_sputnik_dao_factory_result.raw_bytes().unwrap())
+        );
+    }
+    assert!(init_sputnik_dao_factory_result.is_success());
+
+
+}
