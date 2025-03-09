0d2605a6c652308e1a60eab41f9f44589394f018
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-03-09 08:15:42 +0000

    not needed to install all this

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y pkg-config
-sudo apt install -y clang
-rustup target add wasm32-unknown-unknown
-
-# Install Binaryen
-wget https://github.com/WebAssembly/binaryen/releases/download/version_121/binaryen-version_121-x86_64-linux.tar.gz
-tar -xvzf binaryen-version_121-x86_64-linux.tar.gz 
-echo 'export PATH="$(pwd)/binaryen-version_121/bin:$PATH"' >> ~/.bashrc
