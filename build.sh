d44450b7fc4864df1e313efbe0d62b290133d56a
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-03-22 08:43:30 +0000

    enable reproducible build

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-(cd sputnik-staking && cargo near build non-reproducible-wasm --no-abi)
-(cd sputnikdao2 && cargo near build non-reproducible-wasm --no-abi)
-(cd sputnikdao-factory2 && cargo near build non-reproducible-wasm --no-abi)
-(cd test-token && cargo near build non-reproducible-wasm --no-abi)
+(cd sputnik-staking && cargo near build reproducible-wasm)
+(cd sputnikdao2 && cargo near build reproducible-wasm)
+(cd sputnikdao-factory2 && cargo near build reproducible-wasm)
+(cd test-token && cargo near build reproducible-wasm)
 cp target/near/sputnik_staking/sputnik_staking.wasm ./sputnik-staking/res/
 cp target/near/sputnikdao2/sputnikdao2.wasm ./sputnikdao2/res/
 cp target/near/sputnikdao_factory2/sputnikdao_factory2.wasm ./sputnikdao-factory2/res/
--- sputnikdao-factory2/build.sh
+++ sputnikdao-factory2/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-cargo near build non-reproducible-wasm --no-abi
+cargo near build reproducible-wasm
 cp ../target/near/sputnikdao_factory2/sputnikdao_factory2.wasm ./res/
